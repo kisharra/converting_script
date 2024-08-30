89279950ea0b47b0bc3565b5d4d224b2fb913703
Илья Павлов <KISHARRA@satdv.local>
2024-08-30 16:09:59 +1000

    change Db_querry, add more errors hendlers

--- conversion/get_info.py
+++ conversion/get_info.py
@@ -8,67 +8,32 @@ from conversion.db_querry import Db_querry
 
 class Get_Info:
     def __init__(self, config):
-        """
-        Initialize class variables
-
-        Parameters
-        ----------
-        config : dict
-            config dictionary
-
-        Attributes
-        ----------
-        config : dict
-            config dictionary
-        db_file : str
-            path to database file
-        ffprobe_command : list of str
-            list of arguments for ffprobe command
-        log_file : str
-            path to log file
-        db_querry : Db_querry
-            instance of Db_querry class
-        """
         self.config = config
         self.db_file = config['db_file']
         self.ffprobe_command = config['ffprobe_command']
         self.log_file = config['log_file']
-        logging.basicConfig(filename=self.log_file, level=logging.ERROR, format='%(asctime)s:%(message)s')  #logging to file
+        logging.basicConfig(filename=self.log_file, level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')  # updated logging level
         self.db_querry = Db_querry(config)
 
     def get_video_info(self, directory):
-        """
-        Get video info in directory
-
-        Parameters
-        ----------
-        directory : str
-            path to directory
-
-        Returns
-        -------
-        video_files : list
-            list of video files
-        subdirectories : list
-            list of subdirectories
-        """
-
         video_files = []
         subdirectories = []
-        for root, dirs, files in os.walk(directory):  #iterate through directory
+        for root, dirs, files in os.walk(directory):
             if root[-2:] in ("/A", "/0", "/1", "/2", "/3", "/4", "/5", "/6", "/7", "/8", "/9",
                             "/B", "/C", "/D", "/E", "/F", "/G", "/H", "/I", "/J", "/K",
                             "/L", "/M", "/N", "/O", "/P", "/Q", "/R", "/S", "/T", "/U",
                             "/V", "/W", "/X", "/Y", "/Z", "/temp"):
-                for file in files:  #iterate through files
-                    file_path = os.path.join(root, file)  #create file path
-                    video_files.append(file_path)  #add file to list
-                    video_info = self.run_ffprobe(file_path)  #run ffprobe command
-                    if video_info:  #check if video info is not empty
-                        streams = self.streams_data(video_info)  #transform streams data
-                        self.db_querry.save_films(video_info, streams)  #save data to database
+                for file in files:
+                    file_path = os.path.join(root, file)
+                    video_files.append(file_path)
+                    video_info = self.run_ffprobe(file_path)
+                    if video_info:
+                        streams = self.streams_data(video_info)
+                        self.db_querry.save_file_data(video_info, streams, is_film=True, is_serial=False)
+                    else:
+                        logging.warning(f"No video info for file: {file_path}")
                                 
-                for subdir in dirs:  #iterate through subdirectories
+                for subdir in dirs:
                     subdir_path = os.path.join(root, subdir)
                     if os.path.isdir(subdir_path):
                         for sub_root, sub_dirs, sub_files in os.walk(subdir_path):
@@ -77,51 +42,29 @@ class Get_Info:
                                 video_info = self.run_ffprobe(sub_file_path)
                                 if video_info:
                                     streams = self.streams_data(video_info)
-                                    self.db_querry.save_serials(video_info, streams)
+                                    self.db_querry.save_file_data(video_info, streams, is_film=False, is_serial=True)
+                                else:
+                                    logging.warning(f"No video info for file: {sub_file_path}")
 
         return video_files, subdirectories
 
-
     def run_ffprobe(self, file_path):
-        '''
-        Run ffprobe command
-
-        Parameters
-        ----------
-        file_path : str
-            path to file
-
-        Returns
-        -------
-        video_info : dict
-            video info or None if an error occurs
-        '''
-
         command = [arg.format(file_path=file_path) for arg in self.ffprobe_command]
         result = subprocess.run(command, capture_output=True, text=True)
         
         if result.returncode == 0:
-            return json.loads(result.stdout)
+            try:
+                return json.loads(result.stdout)
+            except json.JSONDecodeError:
+                logging.error(f"Error decoding JSON for {file_path}: {result.stdout}")
+                return None
         else:
             logging.error(f"Error running ffprobe for {file_path}: {result.stderr}")
-            return 
+            return None
 
     def streams_data(self, data):
-        '''
-        Transform streams data to get only audio and video streams
-
-        Parameters
-        ----------
-        data : dict
-            video info
-
-        Returns
-        -------
-        transformed_streams : list
-            list of transformed streams
-        '''
         transformed_streams = []
-        for stream in data['streams']:  
+        for stream in data.get('streams', []):
             transformed_stream = {
                 'index': stream['index'],
                 'codec_name': stream['codec_name'],
