d5fd9b75af3f948526b445639f83de784bfb4ba9
Илья Павлов <KISHARRA@satdv.local>
2024-08-30 12:06:24 +1000

    update config path

--- start_conversion.py
+++ start_conversion.py
@@ -5,11 +5,11 @@ import json
 
 
 def load_config(config_file):
-    with open ('/opt/converion/settings/config.json', 'r') as config_file:  #read config file
+    with open ('settings/config.json', 'r') as config_file:  #read config file
         config = json.load(config_file)
     config['maria_db']['password'] = os.getenv('DB_PASSWORD')
     return config
-conversion = ConvertTask(load_config('/opt/converion/settings/config.json'))  #initialize class
+conversion = ConvertTask(load_config('settings/config.json'))  #initialize class
 conversion.convert_files()
 
 
