220a9fa822c8ef659b4468f241eee328063df63d
Илья Павлов <KISHARRA@satdv.local>
2024-08-30 12:01:55 +1000

    update config path

--- add_data_to_db.py
+++ add_data_to_db.py
@@ -4,10 +4,10 @@ import json
 
 
 def load_config(config_file):
-    with open ('settings/config.json', 'r') as config_file:  #read config file
+    with open ('/settings/config.json', 'r') as config_file:  #read config file
         config = json.load(config_file)
     return config
 
-get_info = Get_Info(load_config('config.json'))
-get_info.get_video_info(load_config('config.json')['directory'])
+get_info = Get_Info(load_config('/settings/config.json'))
+get_info.get_video_info(load_config('/settings/config.json')['directory'])
 
