a1e1446ba364f6f07e2d868e957f041b1ade5f66
Илья Павлов <KISHARRA@satdv.local>
2024-08-30 12:03:43 +1000

    update config path

--- create_db_tabels.py
+++ create_db_tabels.py
@@ -4,10 +4,10 @@ import json
 
 
 def load_config(config_file):
-    with open ('/settings/config.json', 'r') as config_file:  #read config file
+    with open ('/opt/converion/settings/config.json', 'r') as config_file:  #read config file
         config = json.load(config_file)
     return config
 
-querry = Db_querry(load_config('/settings/config.json'))
+querry = Db_querry(load_config('/opt/converion/settings/config.json'))
 
 querry.create_table()
